7d3134eece9ce3fd578710090fa96dcaa0ccb93a
Suzuki Kotaro <50207951+Kotarosz727@users.noreply.github.com>
2022-07-29 14:22:48 +0000

    add size

--- 03_decorator_pattern/Bevarage.java
+++ 03_decorator_pattern/Bevarage.java
@@ -1,8 +1,14 @@
 public abstract class Bevarage {
+    public enum Size { TALL, GRANDE, VENTI };
+	Size size = Size.TALL;
     String description = "Unknown Bevarage";
 
     public String getDescription() {
         return description;
+    }
+
+    public size getSize() {
+        return this.size;
     }
 
     public abstract double cost();
