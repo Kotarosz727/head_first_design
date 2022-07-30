27f24fa22f78a9a10f52ffc15557fefff1a7984f
Suzuki Kotaro <50207951+Kotarosz727@users.noreply.github.com>
2022-07-30 14:28:36 +0000

    factory classのスーパークラス

---
+++ 04_factory_pattern/PHP/PizzaStore.php
@@ -0,0 +1,15 @@
+<?php
+namespace PHP;
+
+abstract class PizzaStore
+{
+    public function orderPizza(String $type): Pizza
+    {
+         $pizza = $this->createPizza($type);  
+         $pizza->prepare();
+
+         return $pizza;
+    }
+
+    public abstract function createPizza(String $type);
+}
