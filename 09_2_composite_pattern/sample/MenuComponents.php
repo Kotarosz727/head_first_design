a1462f3db3f182b97113111bf6e4ff477f050d3b
Suzuki Kotaro <50207951+Kotarosz727@users.noreply.github.com>
2022-08-13 15:35:57 +0000

    super class

---
+++ 09_2_composite_pattern/sample/MenuComponents.php
@@ -0,0 +1,24 @@
+<?php
+namespace sample;
+
+class MenuComponents
+{
+    public function add(MenuComponents $data){
+        throw new \Exception('This is not Supoorted');
+    }
+
+    public function remove()
+    {
+        throw new \Exception('This is not Supoorted');
+    }
+
+    public function print()
+    {
+        throw new \Exception('This is not Supoorted');
+    }
+
+    public function get()
+    {
+        throw new \Exception('This is not Supoorted');
+    }
+}
